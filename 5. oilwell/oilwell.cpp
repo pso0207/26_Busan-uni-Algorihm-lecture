c9bdd102656781f8010ad21569dd9289556e599b
pso0207 <qkrtkddnjs0207@naver.com>
2026-03-27 23:10:07 +0900

    과제5번 설명 추가

--- 5. oilwell/oilwell.cpp
+++ 5. oilwell/oilwell.cpp
@@ -1,28 +1,56 @@
 #include <iostream>
-#include "oil.h" 
+#include "oil.h"
 
 using namespace std;
 
-int findStart(int low, int high)
+int find_start(int low, int high)
 {
+    while (low < high)
+    {
+        int mid = (low + high) / 2;
+        int result = observe(low, mid);
 
+        if (result == 0)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid;
+        }
+    }
+    return low;
 }
 
-int findEnd(int low, int high)
+int find_end(int low, int high)
 {
-    
+    while (high > low)
+    {
+        int mid = (low + high) / 2;
+        if (observe(mid + 1, high) == 0)
+        {
+            high = mid;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return high;
 }
 
-int main() {
-
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     initial();
 
     int cellN = oil_size();
-    
+
     int start = 0;
-    int end = cellN-1;
-    int mid = (start + end) /2;
-    // 유정의 시작점 먼저 잡고 끝점 잡기
+    int end = cellN - 1;
+    start = find_start(start, end);
+    end = find_end(start, end);
     oil_report(start, end);
     return 0;
 }
